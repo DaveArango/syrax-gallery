bc20bb26879fc68b169bf28494155c2395a7f445
DaveArango <jhonatandavidvivasarango@gmail.com>
2026-09-10 14:48:22 -0500

    Creacíon arquetipo inicial arquitectura hexagonal.

---
+++ src/main/java/com/uniquindio/syrax_gallery/SyraxGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.syrax_gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyraxGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyraxGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniquindio/syrax_gallery/SyraxGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.syrax_gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyraxGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
